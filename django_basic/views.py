abe74531bff66f73bbcddf2d4e25510a4ca8ac0b
Evair Macollunco <mmacolluncoc@uni.pe>
2021-07-03 00:59:50 -0500

    showed the time of server

--- django_basic/views.py
+++ django_basic/views.py
@@ -1,7 +1,12 @@
 """ My first views with django"""
+# django
 from django.http import HttpResponse
+
+# utilities
+from datetime import datetime
 
 
 def hello_world(request):
-    return HttpResponse('Hello World!!')
+    now = datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+    return HttpResponse(f'The time server is {now}')
 
