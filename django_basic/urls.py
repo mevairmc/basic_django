53a452d5dd4dcfe24977415447d682f17c449a6f
Evair Macollunco <mmacolluncoc@uni.pe>
2021-07-03 19:06:24 -0500

    we create a API that recieve tow extra params

--- django_basic/urls.py
+++ django_basic/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from django_basic import views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world, name='hello_word')
+    path('hello-world/', views.hello_world, name='hello_word'),
+    path('sorted/', views.hi, name="hi"),
+    path('say_hi/<str:name>/<int:age>/', views.say_hi)
 ]
--- django_basic/views.py
+++ django_basic/views.py
@@ -4,9 +4,33 @@ from django.http import HttpResponse
 
 # utilities
 from datetime import datetime
+import json
 
 
 def hello_world(request):
     now = datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
     return HttpResponse(f'The time server is {now}')
 
+
+def hi(request):
+    print(request.GET.get('numbers'))
+    print(request.GET.getlist('numbers'))
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    numbers_sorted = sorted(numbers)
+    response = {
+        'status': 'ok',
+        'numbers': numbers_sorted,
+        'message': 'Integers sorted successfully!!!'
+    }
+    return HttpResponse(
+        json.dumps(response, indent=4),
+        content_type="application/json"
+    )
+
+
+def say_hi(request, name, age):
+    if age < 12:
+        message = f'Sorry {name}, you are not allowed here'
+    else:
+        message = f'Hello {name}, Welcome to Django'
+    return HttpResponse(message)
